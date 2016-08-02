f2d43a024fb3e6b6f21040d4116886d31ae3a5af
Satoshi Nakajima <satoshi.nakajima@gmail.com>
2016-08-02 19:50:06 +0900

    lineTo

---
+++ src/SNDrawView.swift
@@ -0,0 +1,53 @@
+//
+//  SNDrawView.swift
+//  SNDraw
+//
+//  Created by satoshi on 8/2/16.
+//  Copyright © 2016 Satoshi Nakajima. All rights reserved.
+//
+
+import UIKit
+
+class SNDrawView: UIView {
+    var path = CGPathCreateMutable()
+    lazy var shapeLayer:CAShapeLayer = {
+        let shapeLayer = CAShapeLayer()
+        shapeLayer.contentsScale = UIScreen.mainScreen().scale
+        shapeLayer.lineWidth = 10.0
+        shapeLayer.strokeColor = UIColor.blueColor().CGColor
+        shapeLayer.lineCap = "round"
+        shapeLayer.lineJoin = "round"
+        shapeLayer.fillColor = UIColor.clearColor().CGColor
+        self.layer.addSublayer(shapeLayer)
+        return shapeLayer
+    }()
+    
+    override func touchesBegan(touches: Set<UITouch>, withEvent event: UIEvent?) {
+        if let touch = touches.first {
+            let pt = touch.locationInView(self)
+            path = CGPathCreateMutable()
+            CGPathMoveToPoint(path, nil, pt.x, pt.y)
+            shapeLayer.path = path
+        }
+    }
+    
+    override func touchesMoved(touches: Set<UITouch>, withEvent event: UIEvent?) {
+        if let touch = touches.first {
+            let pt = touch.locationInView(self)
+            CGPathAddLineToPoint(path, nil, pt.x, pt.y)
+            shapeLayer.path = path
+        }
+    }
+    
+    override func touchesEnded(touches: Set<UITouch>, withEvent event: UIEvent?) {
+        if let touch = touches.first {
+            let pt = touch.locationInView(self)
+            CGPathAddLineToPoint(path, nil, pt.x, pt.y)
+            shapeLayer.path = path
+        }
+    }
+    
+    override func touchesCancelled(touches: Set<UITouch>?, withEvent event: UIEvent?) {
+        print("touchesCancelled")
+    }
+}
