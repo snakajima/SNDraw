b033a88fcf2e50d7257cc0fc004ed7188ceb8719
Satoshi Nakajima <satoshi.nakajima@gmail.com>
2016-08-02 20:15:37 +0900

    delta

--- src/SNDrawView.swift
+++ src/SNDrawView.swift
@@ -9,7 +9,10 @@
 import UIKit
 
 class SNDrawView: UIView {
+    var delta = 20.0 as CGFloat
     var path = CGPathCreateMutable()
+    var ptLast = CGPointZero
+    var count = 0
     lazy var shapeLayer:CAShapeLayer = {
         let shapeLayer = CAShapeLayer()
         shapeLayer.contentsScale = UIScreen.mainScreen().scale
@@ -28,21 +31,29 @@ class SNDrawView: UIView {
             path = CGPathCreateMutable()
             CGPathMoveToPoint(path, nil, pt.x, pt.y)
             shapeLayer.path = path
+            ptLast = pt
+            count = 0
         }
     }
     
     override func touchesMoved(touches: Set<UITouch>, withEvent event: UIEvent?) {
         if let touch = touches.first {
             let pt = touch.locationInView(self)
-            CGPathAddLineToPoint(path, nil, pt.x, pt.y)
-            shapeLayer.path = path
+            let (dx, dy) = (pt.x - ptLast.x, pt.y - ptLast.y)
+            if count > 0 && dx * dx + dy * dy > delta * delta {
+                let ptMid = CGPointMake((ptLast.x + pt.x) / 2.0, (ptLast.y + pt.y) / 2.0)
+                CGPathAddQuadCurveToPoint(path, nil, ptLast.x, ptLast.y, ptMid.x, ptMid.y)
+                shapeLayer.path = path
+                ptLast = pt
+            }
+            count = count + 1
         }
     }
     
     override func touchesEnded(touches: Set<UITouch>, withEvent event: UIEvent?) {
         if let touch = touches.first {
             let pt = touch.locationInView(self)
-            CGPathAddLineToPoint(path, nil, pt.x, pt.y)
+            CGPathAddQuadCurveToPoint(path, nil, ptLast.x, ptLast.y, pt.x, pt.y)
             shapeLayer.path = path
         }
     }
