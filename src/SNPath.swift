57de24cc9140547a2d62b8268a8e6a6355bfd581
Satoshi Nakajima <satoshi.nakajima@gmail.com>
2016-08-06 06:46:58 +0900

    addToPath

--- src/SNPath.swift
+++ src/SNPath.swift
@@ -12,14 +12,7 @@ public struct SNPath {
     static func pathFrom(elements:[SNPathElement]) -> CGPath {
         let path = CGPathCreateMutable()
         for element in elements {
-            switch(element) {
-            case let move as SNMove:
-                CGPathMoveToPoint(path, nil, move.pt.x, move.pt.y)
-            case let curve as SNQuadCurve:
-                CGPathAddQuadCurveToPoint(path, nil, curve.cpt.x, curve.cpt.y, curve.pt.x, curve.pt.y)
-            default:
-                break
-            }
+            element.addToPath(path)
         }
         return path
     }
@@ -27,21 +20,21 @@ public struct SNPath {
     static func polyPathFrom(elements:[SNPathElement]) -> CGPath {
         let path = CGPathCreateMutable()
         for element in elements {
-            switch(element) {
-            case let move as SNMove:
-                CGPathMoveToPoint(path, nil, move.pt.x, move.pt.y)
-            case let curve as SNQuadCurve:
-                CGPathAddLineToPoint(path, nil, curve.cpt.x, curve.cpt.y)
-                CGPathAddLineToPoint(path, nil, curve.pt.x, curve.pt.y)
-            default:
-                break
-            }
+            element.addToPathAsPolygon(path)
         }
         return path
     }
 }
 
 public protocol SNPathElement {
+    func addToPath(path:CGMutablePath)
+    func addToPathAsPolygon(path:CGMutablePath)
+}
+
+extension SNPathElement {
+    public func addToPathAsPolygon(path:CGMutablePath) {
+        addToPath(path)
+    }
 }
 
 public struct SNMove:SNPathElement {
@@ -49,10 +42,18 @@ public struct SNMove:SNPathElement {
     init(x:CGFloat, y:CGFloat) {
         pt = CGPointMake(x,y)
     }
+    
+    public func addToPath(path:CGMutablePath) {
+        CGPathMoveToPoint(path, nil, pt.x, pt.y)
+    }
 }
 
 public struct SNLine:SNPathElement {
     let pt:CGPoint
+
+    public func addToPath(path:CGMutablePath) {
+        CGPathAddLineToPoint(path, nil, pt.x, pt.y)
+    }
 }
 
 public struct SNQuadCurve:SNPathElement {
@@ -61,6 +62,15 @@ public struct SNQuadCurve:SNPathElement {
     init(cpx: CGFloat, cpy: CGFloat, x: CGFloat, y: CGFloat) {
         cpt = CGPointMake(cpx, cpy)
         pt = CGPointMake(x, y)
+    }
+
+    public func addToPath(path:CGMutablePath) {
+        CGPathAddQuadCurveToPoint(path, nil, cpt.x, cpt.y, pt.x, pt.y)
+    }
+
+    public func addToPathAsPolygon(path:CGMutablePath) {
+        CGPathAddLineToPoint(path, nil, cpt.x, cpt.y)
+        CGPathAddLineToPoint(path, nil, pt.x, pt.y)
     }
 }
 
