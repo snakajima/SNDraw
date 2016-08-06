90a9a2be83266cdcd02f24c3b716f5fc646eb4b1
Satoshi Nakajima <satoshi.nakajima@gmail.com>
2016-08-06 18:42:53 +0900

    layers

--- sample/SNDraw/SNDraw2/ViewController.swift
+++ sample/SNDraw/SNDraw2/ViewController.swift
@@ -8,11 +8,38 @@
 
 import UIKit
 
+enum DrawStatus: Int {
+    case none = 0
+    case invalid = 1
+    case valid = 2
+    case drawing = 3
+    case done = 4
+}
+
 class ViewController: UIViewController {
+    private let status = DrawStatus.none
+    private let layers = [CALayer(), CALayer()]
+    private let invalidColor = UIColor(red: 1, green: 0, blue: 0, alpha: 0.5).CGColor
+    private let validColor = UIColor(red: 0, green: 1, blue: 0, alpha: 0.25).CGColor
+    private let activeColor = UIColor(red: 0, green: 1, blue: 0, alpha: 0.5).CGColor
+    private let radius = 100.0 as CGFloat
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        let frame = CGRectMake(0, 0, radius * 2, radius * 2)
+        layers.forEach() { (layer: CALayer) in
+            layer.frame = frame
+            layer.backgroundColor = validColor
+            layer.cornerRadius = radius
+            self.view.layer.addSublayer(layer)
+        }
+    }
+    
+    override func viewDidLayoutSubviews() {
+        let size = self.view.frame.size
+        layers[0].position = CGPointMake(0, size.height)
+        layers[1].position = CGPointMake(size.width, size.height)
     }
 
     override func didReceiveMemoryWarning() {
